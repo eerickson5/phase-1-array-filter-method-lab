88a2bd2f264020e7f36943244e5c272fdc787d10
Elise Erickson <elisece717@gmail.com>
2023-11-24 07:37:29 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+findMatching = (drivers, name) => { 
+    return(drivers.filter( driver => driver.toLowerCase() === name.toLowerCase()))
+}
+
+fuzzyMatch = (drivers, name) => {
+    return drivers.filter( driver => driver.toLowerCase().indexOf(name.toLowerCase()) === 0)
+}
+
+matchName = (drivers, name) => {
+    return drivers.filter( driver => driver.name.toLowerCase() === name.toLowerCase())
+}
